519da2029b4822789a67d1db7db258bd97cafd0f
SaVance Ford <savancef@gmail.com>
2024-06-29 22:17:55 -0500

    Update nav responsiveness, add hamburger menu for mobile screens, add open and close state for hamburger menu and nav list items

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export function Navigation() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav>
       <div>
@@ -9,7 +12,25 @@ export function Navigation() {
         </div>
         <p>SaVance Ford</p>
       </div>
-      <ul>
+      <ul className='desktop-nav'>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/experience'>Experience</Link>
+        </li>
+      </ul>
+      <div className='hamburger-container'>
+        <div
+          className={isMenuOpen ? 'rotated-menu' : 'hamburger-menu'}
+          onClick={() => setMenuOpen(!isMenuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <ul className={isMenuOpen ? 'mobile-list' : 'mobile-list-state-1'}>
         <li>
           <Link to='/'>Home</Link>
         </li>
